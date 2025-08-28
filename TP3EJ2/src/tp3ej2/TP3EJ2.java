bf5ea59ce4e75b728e98f12f4789e05949c15e63
Anna Laura Carrizo V <carrizovillarrealanna@gmail.com>
2025-08-28 19:37:01 -0300

    primer commint

--- TP3EJ2/src/tp3ej2/TP3EJ2.java
+++ TP3EJ2/src/tp3ej2/TP3EJ2.java
@@ -15,7 +15,7 @@ public class TP3EJ2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("??");
     }
     
 }
